64bfe29b0592242935910cef2b2f3541648d1f3d
AlbertoHori <57484765+AlbertoHori@users.noreply.github.com>
2019-11-11 15:41:47 +0100

    Add files via upload

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -2,6 +2,8 @@ import React from "react";
 
 const Checkbox = props => (
   <input type="checkbox" {...props}
+  key={props.id}
+  name={props.index}
   checked={props.chck}
   onClick={props.clicked}
   value={props.value}
